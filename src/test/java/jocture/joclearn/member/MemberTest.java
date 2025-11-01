818dfe5ccd60009de1d0c8ee8a04dc30e2e80ea9
Ggyumalang <khg2154@naver.com>
2025-11-01 11:39:16 +0900

    2-2. 회원 도메인 모델 작성(2) - 도메인 로직 메서드 추가

--- src/test/java/jocture/joclearn/member/MemberTest.java
+++ src/test/java/jocture/joclearn/member/MemberTest.java
@@ -1,14 +1,33 @@
 package jocture.joclearn.member;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class MemberTest {
 
+    Member member;
+
+    @BeforeEach
+    void setup() {
+        member = Member.create("khg5087", "khg5087@naver.com", "abcd1234");
+    }
+
     @Test
-    void member() {
-        Member member = new Member("khg5087", "khg5087@naver.com", "abcd1234");
+    void create() {
         assertThat(member.getStatus()).isEqualTo(MemberStatus.STANDBY);
+    }
+
+    @Test
+    void activate() {
+        member.activate();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void deactivate() {
+        member.deactivate();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.INACTIVE);
     }
 }
